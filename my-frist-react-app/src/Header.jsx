19b1ebe9ac36506a9ec7efa66cbea07e9367f7aa
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-03-17 13:08:42 +0430

    chenges

---
+++ my-frist-react-app/src/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    let name = "Nazanin"
+    return(
+        <>
+        <h1>Hi there Iam {name}</h1>
+        <h3>Hi there Iam comming form Header componend </h3>
+        </>
+    )
+}
+export default Header;
