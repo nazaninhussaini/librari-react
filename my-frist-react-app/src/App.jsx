0e0618d732cede7f4f594471ddf13c4a9cb9157f
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-03-18 13:28:58 +0430

    chenges

--- my-frist-react-app/src/App.jsx
+++ my-frist-react-app/src/App.jsx
@@ -1,12 +1,17 @@
-import Header from "./Header";
-
-function App(){
-  return(
-    <>
-    <h1>Hello World</h1>
-    <Header/>
-    </>
-  )
-
+import Hero from "./Header";
+function App (){
+  let name = "nazanin"
+  let lastName = "Hussaini"
+  let age = 18;
+  <>
+  <h1>
+    {" "}
+    Hi Iam {name} my last name {lastName} iam {age} years old
+  </h1>
+  <Hero name="Mohammad" lastName= "Husaaini" />
+  <Hero name="Ali" lastName= "Khamosh" />
+  <Hero name="Ahmad" lastName= "Sadat" />
+  <Hero name="Farid" lastName= "Khorami" />
+  </>
 }
 export default App;
--- my-frist-react-app/src/Header.jsx
+++ my-frist-react-app/src/Header.jsx
@@ -1,10 +1,3 @@
-function Header(){
-    let name = "Nazanin"
-    return(
-        <>
-        <h1>Hi there Iam {name}</h1>
-        <h3>Hi there Iam comming form Header componend </h3>
-        </>
-    )
+export default function Hero (props){
+    return <div>Hi Iam {props.name} {props.lastName}</div> 
 }
-export default Header;
