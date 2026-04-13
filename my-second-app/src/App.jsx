9678be84c7d43603b6222781149f4260de924c75
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-04-13 18:51:14 +0430

    chenges

--- my-second-app/src/App.jsx
+++ my-second-app/src/App.jsx
@@ -1,3 +1,4 @@
+import { Cloud, CloudDrizzle, CloudFog, CloudRain, CloudSun, Snowflake, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 
 function App(){
@@ -19,8 +20,16 @@ function App(){
     <div>
 
       <div style={{width:"100%",height:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
-        <div style={{width:"50%",height:"fit-content",padding:"20px",border:"1px solid black ",borderRadius:"5px"}}>
-
+        <div style={{width:"50%",height:"fit-content",padding:"20px",border:"1px solid black ",borderRadius:"5px",display:"flex",justifyContent:"center",alignItems:"center"}}>
+          <div style={{width:"50%"}}>
+          {weatherData.weather[0].main=== "Clear"?(<Sun size={42} style={{color:"yellow"}}/>):
+          weatherData.weather[0].main === "Clousd"?(<Cloud size={42} />):
+          weatherData.weather[0].main === "Thunderstorm"?(<CloudSun size={42}/>) :
+          weatherData.weather[0].main === "Drizzle"? (<CloudDrizzle size={42}/>):
+          weatherData.weather[0].main === "Rain" ? (<CloudRain size={42}/>):
+          weatherData.weather[0].main === "Snow" ?(<Snowflake size={42}/>):
+          weatherData.weather[0].main === "Fog" ? (<CloudFog size={42}/>):""}
+        </div>
         </div>
 
       </div>
