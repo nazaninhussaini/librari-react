0a26350acfe56875e196ebb0138b4ecf4b24efea
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-04-16 20:24:13 +0430

    chenges:

--- my-second-app/src/App.jsx
+++ my-second-app/src/App.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 function App(){
   const [weatherData,setWeatherData] = useState(null);
+  const [cityName,setCityName] = useState("Kabul");
+  const [isButtonClicked,setIsButtonClicked]= useState(false)
   const [hour ,setHour] = useState(" ");
   const [minute ,setMinute] = useState(" ");
   const [seconds ,setSeconds] = useState(" ");
@@ -11,10 +13,12 @@ function App(){
   const [sunSetMinute,setSunSetMinute] = useState("");
   const [sunSetSeconds,setSunSetSeconds] = useState("");
 
-
+  function handelClick(){
+    setIsButtonClicked(!isButtonClicked)
+  }
   useEffect(()=>{
    async function getWeather(){
-    const data = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Kabul&units=metric&appid=684418efa9274f3ad6491868b0271123");
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=684418efa9274f3ad6491868b0271123`);
     const javab = await data.json();
     setWeatherData(javab);
 
@@ -37,17 +41,25 @@ function App(){
 
     }
     getWeather()
-  },[]);
+  },[isButtonClicked]);
   if(!weatherData){
     return(
-      <h1 style={{fontSize:"40px",fontWeight:"bold", color:"red",padding:"12px",textAlign:"center"}}>Please a minute.....</h1>
+      <h1 style={{fontSize:"40px",fontWeight:"bold", color:"red",padding:"12px",textAlign:"center"}}>Please with a minute.....</h1>
     )
   }
   return(
     <div>
 
-      <div style={{width:"100%",height:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
-        <div style={{width:"50%",height:"fit-content",padding:"20px",border:"1px solid black ",borderRadius:"5px",display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <div style={{width:"100%",height:"100vh",display:"flex",flexDirection:"column",gap:"10px" ,justifyContent:"center",alignItems:"center", backgroundColor:"gray"}}>
+      <div style={{width:"50%",display:"flex"}}>
+        <input type="text" style={{border:"1px solid black",width:"100%"}}
+        value={cityName} onChange={(e)=>setCityName(e.target.value)}
+        />
+        <button style={{padding:"6px 12px" ,backgroundColor:"blue",borderRadius:"5px", color:"white"}} 
+        onClick={handelClick}
+        >Search</button>
+      </div>
+        <div style={{width:"50%",height:"fit-content",padding:"20px",border:"1px solid black ",borderRadius:"5px",display:"flex",justifyContent:"center",alignItems:"center",boxShadow:"2px 2px 10px black"}}>
           <div style={{width:"50%"}}>
           {weatherData.weather[0].main=== "Clear"?(<Sun size={42} style={{color:"yellow"}}/>):
           weatherData.weather[0].main === "Clousd"?(<Cloud size={42} />):
@@ -60,7 +72,7 @@ function App(){
         <div style={{display:"flex",width:"100%",justifyContent:"space-between",alignItems:"center"}}> 
           <p style={{display:"flex" , gap:"8px"}}>
             <span>Tempreture</span>
-            <span>{weatherData.main.temp}°</span>
+            <span>{weatherData.main.temp}°C</span>
           </p>
           <p style={{display:"flex",gap:"8px"}}>
             <span>Humidity</span>
