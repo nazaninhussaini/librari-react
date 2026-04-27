04b411a94ab449a6dc5a87450d5abb6d43450150
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-04-27 11:51:25 +0430

    image-chenges

--- Example-app/src/Navbar.jsx
+++ Example-app/src/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Nav({showForm,setShowForm}){
         <nav className="w-full bg-linear-90 from-gray-800 to-gray-600 fixed top-0 left-0">
             <div className="w-full flex justify-between p-3 items-center gap-2 text-2xl text-teal-300  shadow-teal-300 ">
                 {/* <span className="flex text-center items-center" ><Moon/></span> */}
-                <img src="../img/p1.jpeg" alt="" className="border w-12 rounded-full h-12 shadow-2xl shadow-teal-300" />
+                <img src="../img/p2.jpg" alt="" className="border w-15 rounded-full h-12 shadow-2xl shadow-teal-300" />
                 <h1 className="flex w-full text-center ">Nazanin.Dev</h1>
                 <ul className="flex gap-5 mx-5">
                 <li>Home</li>
