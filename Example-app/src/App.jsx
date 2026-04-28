0b50fcdeafe84788fa604dd132b51d7239bc7e5f
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-04-28 11:28:34 +0430

    add codes

--- Example-app/src/App.jsx
+++ Example-app/src/App.jsx
@@ -8,7 +8,7 @@ function App(){
   const [list,setList] = useState([]);
   console.log(list)
   return(
-    <div  className="w-full h-screen  bg-linear-90 from-gray-800 to-gray-700 ">
+    <div  className="w-full [h-2screen]  bg-linear-90 from-gray-800 to-gray-700 ">
       <Nav showForm = {showForm} setShowForm = {setShowForm}/>
       <CreatForm list= {list} setList= {setList} showForm = {showForm} setShowForm={setShowForm}  />
     <TodoList list={list} setList={setList}/>
