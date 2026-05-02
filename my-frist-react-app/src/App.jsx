85e857a7fc3380d0de543ff4a14d6c9000b54493
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-05-02 20:16:39 +0430

    use Reduser hooks added

--- my-frist-react-app/src/App.jsx
+++ my-frist-react-app/src/App.jsx
@@ -1,17 +1,30 @@
-import { createContext } from "react";
-import Home from "./Home";
-import { list } from "../db/student";
-export const StudentContext = createContext();
-
+import { useReducer } from "react";
+let intialValue = 0;
+function reducer (state,action){
+  if(action.type === "increament"){
+    return state + 1;
+  }
+  else if(action.type === "delete"){
+    return state = intialValue;
+  }
+  else{
+    return state - 1;
+  }
+}
 function App(){
-  const studentList = list;
+  const [state,func] = useReducer(reducer,intialValue);
   return(
-    <div>
-      <h1 className="w-full flex justify-center text-4xl font-medium text-gray-600 pt-3 ">Home...</h1>
-
-      <StudentContext.Provider value={studentList}>
-      <Home/> 
-      </StudentContext.Provider>
+  <div className="w-full flex justify-center py-3  ">
+      <div className="flex w-40 justify-center gap-3 flex-col">
+      <button 
+      className=" rounded-sm p-3 font-normal text-xl text-stone-100 bg-linear-60 from-amber-400 to-red-500 "
+      onClick={()=>func({type:"increament"})}>Increament</button>
+      <h1 className="text-center text-xl">{state}</h1>
+      <button className=" text-xl font-normal bg-linear-60 from-amber-400 to-red-500 text-stone-100 rounded-sm p-3 "
+       onClick={()=>func({type :"decrement"})}>Decreament</button>
+      <button className=" text-xl font-normal bg-linear-60 from-amber-400 to-red-500 text-stone-100 rounded-sm p-3 "
+       onClick={()=>func({type :"delete"})}>Reset</button>
+      </div>
     </div>
   )
 }
