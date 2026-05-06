fcd54074f10eca7372a564a7169d2ea882c545df
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-05-06 12:12:09 +0430

    Added codes for app

--- my-frist-react-app/src/App.jsx
+++ my-frist-react-app/src/App.jsx
@@ -1,19 +1,26 @@
-import { useReducer, useState } from "react";
-let intialValue = {count:0};
+import { useReducer, useRef, useState } from "react";
+const intialValue = { count:0 };
 
 function App(){
 
 function reducer (state,action){
   switch(action.type){
     case "increament":
-    return{count: state.count + action.paylod }
+    return{count: state.count + action.payload }
     default :
-     return{count: state.count -  action.paylod }
+     return{count: state.count -  action.payload }
   }
 }
+function handelIncreament (){
+  Ref.current ++;
+}
+function handelDecrement(){
+  Ref.current -- ;
+}
 
+  const Ref =  useRef (0);
   const [state,func] = useReducer(reducer,intialValue);
-  const [value,setValue] = useState(0)
+  const [value,setValue] = useState(0);
   return(
   <div className="w-full flex justify-center flex-col items-center gap-4 py-3  ">
     <h1 className="text-center text-3xl bg-linear-60 from-amber-500 to-red-500 text-transparent bg-clip-text">Counter with useReducer</h1>
@@ -21,7 +28,7 @@ function reducer (state,action){
       <button 
       className=" rounded-sm p-2 font-normal text-xl text-stone-100 bg-linear-60 from-amber-400 to-red-500 "
       onClick={()=>func({type:"increament" ,payload: 2})}>Increament</button>
-      <h1 className="text-center text-xl text-amber-500">{state.count}</h1>
+      <h1 className="text-center text-xl text-amber-500">{ state.count }</h1>
       <button className=" text-xl font-normal bg-linear-60 from-amber-400 to-red-500 text-stone-100 rounded-sm p-2 "
        onClick={()=>func({type :"decrement" ,payload : 2})}>Decreament</button>
       {/* <button className=" text-xl font-normal bg-linear-60 from-amber-400 to-red-500 text-stone-100 rounded-sm p-2 "
@@ -37,10 +44,15 @@ function reducer (state,action){
         onClick={()=> setValue (value - 1)}>Decreament</button>
       </div>
       <div className="w-full flex items-center flex-col gap-4 p-2">
-        <h1 className="text-center text-3xl bg-linear-60 from-green-600 to-orange-400  text-transparent bg-clip-text">Counter with useRef</h1>
-        <button className="text-xl font-normal bg-linear-60 from-green-600 to-orange-400 w-40 text-stone-100 rounded-sm p-2">Increament</button>
-        <h1 className="text-center text-xl text-blue-500">0</h1>
-        <button className="text-xl font-normal bg-linear-60 from-green-600  to-orange-400 w-40 text-stone-100 rounded-sm p-2">Decreament</button>
+        <h1 className="text-center text-3xl bg-linear-60 from-green-600 to-orange-400  text-transparent bg-clip-text"
+        >Counter with useRef</h1>
+        <button  className="text-xl font-normal bg-linear-60 from-green-600 to-orange-400 w-40 text-stone-100 rounded-sm p-2"
+        onClick={handelIncreament}
+        >Increament</button>
+        <h1 className="text-center text-xl text-blue-500">{Ref.current}</h1>
+        <button className="text-xl font-normal bg-linear-60 from-green-600  to-orange-400 w-40 text-stone-100 rounded-sm p-2"
+         onClick={handelDecrement}
+        >Decreament</button>
       </div>
 
     </div>
