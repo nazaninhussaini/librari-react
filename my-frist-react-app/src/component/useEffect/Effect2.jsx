f7f23c661e9d0c1ce8014c98e3e7ecf8da17ac2a
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-05-15 21:49:41 +0430

    chenges codes

--- my-frist-react-app/src/component/useEffect/Effect2.jsx
+++ my-frist-react-app/src/component/useEffect/Effect2.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 export default function Effect2 (){
     const [ranning,setRunning] = useState(false);
     const [seconds,setSeconds]= useState(10);
+    const [myChois,setMyChois] = useState(0);
     useEffect(()=>{
         let interval =null;
         if(seconds<=0){
@@ -17,11 +18,23 @@ export default function Effect2 (){
             setSeconds((prev) => prev - 1)
         },1000)}
         return ()=> clearInterval(interval)
-    },[ranning,seconds])
+    },[ranning,seconds]);
+    function func(){
+        setSeconds(myChois)
+    }
     return(
-        <div className="border bg-linear-60 from-blue-400 via-purple-500 p-3 text-transparent bg-clip-text to-black">
-            <div className="border w-full h-fit flex justify-center">
-                <div className="border w-1/2 p-4 ">
+        <div>
+            <div className=" w-full flex justify-center bg-linear-60 from-blue-300 text-white via-gray-400 to-red-500">
+                <div className="border w-full px-4 py-3 flex flex-col gap-3 ">
+                <h1 className="text-center pb-3">Qustion 2</h1>
+                <div className="flex justify-center text-center gap-2 ">
+                <input value={myChois}
+                onChange={(e)=>setMyChois(Number(e.target.value))}
+                type="number" className="border rounded-sm p-2 focus:outline-0 cursor-pointer" placeholder="Please Set Number" />
+                <button 
+                onClick={func}
+                className="border p-2 ">Set Number</button>
+                </div>
                 <h1>{seconds}</h1>
             <div className="flex w-full justify-between ">
                     <button onClick={()=> setRunning(true)} className="py-2 px-5 bg-blue-500 text-white rounded-sm cursor-pointer">Start</button>
